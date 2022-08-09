6cccd7fc18747aabe7ea8611ec65e091b6f5d0e0
Veralee225 <Veralee225@gmail.com>
2022-08-09 17:05:39 +0100

    bug fix

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""module amenity"""
+""" Module for class Amenity """
 
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """class amenity"""
+    """ class that defines all Amenity instances
+    Args:
+        name - name of amenity
+    """
+
     name = ""
